c11ceacd89bb7e904bc7de0077dc22a7295d43c1
Jooa Lee <jooalee@Jooas-MacBook-Pro.local>
2024-06-30 17:31:41 +0900

    Add .gitignore file

--- test.py
+++ test.py
@@ -1,1 +1,23 @@
-# this is test.py file 
+import boto3 # AWS lambda dependencies 
+import sys 
+import os 
+import config
+
+from azure.functions import Function 
+from azure.identity import DefaultAzureCredential 
+
+# def start_migration(source, destination):
+#     s3 = boto3.resource('s3')
+    
+    
+#     # Get the source and destination bucket objects
+#     source = s3.Bucket(source)
+#     print(source)
+#     pass 
+def lambda_handler(event, context):
+    
+def main():
+    start_migration()
+
+if __name__ == "__main__":
+    main() 
